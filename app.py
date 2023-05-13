6ae2342cfcdc3f50437644edfe3ba58a3f980caf
s498 <sathwik.edupuganti@gmail.com>
2023-05-13 12:47:16 -0700

    Code refactored

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ from sklearn import metrics
 from flask import Flask, request, render_template
 import pickle
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 df_1 = pd.read_csv("first_telc.csv")
 
 q = ""
 
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def loadPage():
-	return render_template('home.html', query="")
+    return render_template('home.html', query="")
 
 
 @app.route("/", methods=['POST'])
@@ -44,25 +44,25 @@ def predict():
     tenure
     '''
 
-    inputQuery1 = request.form['query1']
-    inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
-    inputQuery6 = request.form['query6']
-    inputQuery7 = request.form['query7']
-    inputQuery8 = request.form['query8']
-    inputQuery9 = request.form['query9']
-    inputQuery10 = request.form['query10']
-    inputQuery11 = request.form['query11']
-    inputQuery12 = request.form['query12']
-    inputQuery13 = request.form['query13']
-    inputQuery14 = request.form['query14']
-    inputQuery15 = request.form['query15']
-    inputQuery16 = request.form['query16']
-    inputQuery17 = request.form['query17']
-    inputQuery18 = request.form['query18']
-    inputQuery19 = request.form['query19']
+    inputQuery1 = request.form['citizen']
+    inputQuery2 = request.form['monthly-charges']
+    inputQuery3 = request.form['total-charges']
+    inputQuery4 = request.form['gender']
+    inputQuery5 = request.form['partner']
+    inputQuery6 = request.form['dependents']
+    inputQuery7 = request.form['phone-service']
+    inputQuery8 = request.form['multiple-lines']
+    inputQuery9 = request.form['internet-service']
+    inputQuery10 = request.form['online-security']
+    inputQuery11 = request.form['online-backup']
+    inputQuery12 = request.form['device-protect']
+    inputQuery13 = request.form['tect-support']
+    inputQuery14 = request.form['streaming-tv']
+    inputQuery15 = request.form['streaming-movies']
+    inputQuery16 = request.form['contract']
+    inputQuery17 = request.form['paperless-billing']
+    inputQuery18 = request.form['payment-method']
+    inputQuery19 = request.form['tenure']
 
     model = pickle.load(open("model.sav", "rb"))
 
@@ -81,33 +81,14 @@ def predict():
     labels = ["{0} - {1}".format(i, i + 11) for i in range(1, 72, 12)]
 
     df_2['tenure_group'] = pd.cut(df_2.tenure.astype(
-    	int), range(1, 80, 12), right=False, labels=labels)
+        int), range(1, 80, 12), right=False, labels=labels)
     # drop column customerID and tenure
     df_2.drop(columns=['tenure'], axis=1, inplace=True)
 
-    # dummified_col = ['gender','SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
-    #                  'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
-    #                  'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
-    #                  'Contract', 'PaperlessBilling', 'PaymentMethod', 'tenure_group']
-
-    # excluded_col = ['MonthlyCharges', 'TotalCharges']
-
-    # new_df__dummies = pd.get_dummies(df_2.drop(columns=excluded_col),
-    #                                  columns=dummified_col,
-    #                                  drop_first=True)
-
-    # new_df__dummies()
-
-# Insert the excluded columns at the beginning of the dummified dataframe
-    # for column in excluded_col[::-1]:
-    #     new_df__dummies.insert(0, column, df_2[column])
-
     new_df__dummies = pd.get_dummies(df_2, columns=['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
-                                           'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
-                                           'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
-                                           'Contract', 'PaperlessBilling', 'PaymentMethod', 'tenure_group'])
-
-    # new_df__dummies[['SeniorCitizen','MonthlyCharges','TotalCharges']] = df_2[['SeniorCitizen','MonthlyCharges','TotalCharges']]
+                                                    'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
+                                                    'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
+                                                    'Contract', 'PaperlessBilling', 'PaymentMethod', 'tenure_group'])
 
     print(df_1.columns, 'df1 columns')
     print(df_2.columns, 'df_2 col')
@@ -115,38 +96,20 @@ def predict():
     print(new_df__dummies.tail(1).columns, 'columns')
     print(new_df__dummies.tail(1), 'new_df_dum')
 
-    # final_df=pd.concat([new_df__dummies, new_dummy], axis=1)
-
     single = model.predict(new_df__dummies.tail(1))
     probablity = model.predict_proba(new_df__dummies.tail(1))[:, 1]
 
     if single == 1:
-        o1 = "This customer is likely to be churned!!"
+        o1 = "is likely to be churned!!"
         o2 = "Confidence: {}".format(probablity*100)
     else:
-        o1 = "This customer is likely to continue!!"
+        o1 = "is likely to continue!!"
         o2 = "Confidence: {}".format(probablity*100)
 
-    return render_template('home.html', output1=o1, output2=o2,
-                           query1=request.form['query1'],
-                           query2=request.form['query2'],
-                           query3=request.form['query3'],
-                           query4=request.form['query4'],
-                           query5=request.form['query5'],
-                           query6=request.form['query6'],
-                           query7=request.form['query7'],
-                           query8=request.form['query8'],
-                           query9=request.form['query9'],
-                           query10=request.form['query10'],
-                           query11=request.form['query11'],
-                           query12=request.form['query12'],
-                           query13=request.form['query13'],
-                           query14=request.form['query14'],
-                           query15=request.form['query15'],
-                           query16=request.form['query16'],
-                           query17=request.form['query17'],
-                           query18=request.form['query18'],
-                           query19=request.form['query19'])
+    return render_template('output.html', output1=o1, output2=o2,
+                           firstName=request.form['firstName'],
+                           lastName=request.form['lastName'])
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
+Data Source: https://www.kaggle.com/datasets/palashfendarkar/wa-fnusec-telcocustomerchurn
+
 1. python app.py
 2. install any packages that are required as prompted
